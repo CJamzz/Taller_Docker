e8d9d54550022a6d385129db9917643735efbcc9
CJamzz <jorgecamargo689@gmail.com>
2023-03-23 16:45:21 -0500

    Solución trabajo docker

---
+++ src/main/java/co/edu/uan/sofeng/ejemplo/HelloController.java
@@ -0,0 +1,20 @@
+package co.edu.uan.sofeng.ejemplo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController 
+public class HelloController {
+   @GetMapping("/")
+   public String index() {
+      return "Welcome to Spring Boot!";
+   }
+
+   public class SayHello {
+      @GetMapping("/say_hello")
+      public String sayHello(@RequestParam(defaultValue = "World") String name) {
+         return "Welcome to Spring Boot!";
+      }
+   } 
+} 
---
+++ src/test/java/co/edu/uan/sofeng/ejemplo/SayHelloName.java
@@ -0,0 +1,28 @@
+package co.edu.uan.sofeng.ejemplo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import co.edu.uan.sofeng.ejemplo.HelloController.SayHello;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SayHelloName{
+
+   @Autowired
+   private TestRestTemplate template;
+
+   @Test
+   public void getHello() throws Exception {
+
+    SayHello sayHello = new SayHello("Jorge");
+
+      ResponseEntity<String> response = template.getForEntity("/say_hello", String.class);
+      assertThat(response.getBody()).isEqualTo("HelloJorge");
+   }
+}
